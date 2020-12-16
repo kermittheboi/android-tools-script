f35e3dd34d2a4f5f5d8afe3a1ae2660df55c7a76
KermitBoi <73022467+kermittheboi@users.noreply.github.com>
2020-12-16 21:45:57 +0200

    Update tools-uninstallation-script.sh

--- tools-uninstallation-script.sh
+++ tools-uninstallation-script.sh
@@ -9,8 +9,8 @@ NO_COLOR='\033[0m'
 # git uninstall script
 printf "${YELLOW}Removing installed/created files\n${NO_COLOR}"
 sudo apt remove git
-sudo apt autoremove
 sudo apt remove openjdk-8-jdk
+sudo apt autoremove
 rm -r scripts
 rm -r platform-tools
 rm -r ~/bin
